23017b542d54919ba331b8665ce8a4fc0e709348
amine dahak <dhahakamin@gmail.com>
2018-11-09 17:41:29 +0100

    commit test 11/08/2018

--- solution.js
+++ solution.js
@@ -1,104 +1,66 @@
-
-// the solution is developed with a test.
-// open the terminal, go to the directory, execute the command "node solution.js"
-
-//test 1
-function f1 (a , b ) {
-    var array_result = [] ; 
-      for (i=0;i<a.length  ; i++)     {
-        var counter = 0 ; 
-         for( j=0 ;j< b.length;j++) {
-            if (a[i] === b[j] ) {
-                 counter += 1; 
-            }
-         array_result[i]= counter ;    
-         }
-      }
-return array_result ; 
+function f1 (a , b ) {  
+  let counter = 0 ; 
+ return a.map( string_a => { 
+            counter = 0;
+            b.map( string_b => {
+            if(string_a === string_b)  counter +=1; 
+        });
+       return counter ;
+     });
 }
+//uncommented the test 1 and  run  node solution.js in the terminal
 
-//run function f1 
-//example 1 
-var array_a  = ["aba", "xzxb", "ab"];
-var array_b =["aba", "baba", "aba", "xzxb"];
+// var array_a  = ["aba", "xzxb", "ab"];
+// var array_b =["aba", "baba", "aba", "xzxb"];
+// console.log(array_a) ; 
+// console.log(array_b) ; 
+// console.log("run f1(array_a ,array_b)") ; 
+// console.log( f1(array_a ,array_b)) ; 
 
-console.log(array_a) ; 
-console.log(array_b) ; 
-console.log("run f1 with example 1") ; 
-console.log( f1(array_a ,array_b)) ; 
-//example 2
-var  array_d = ["bb","ab","aba","cc","j"] ;
-var  array_x = ["aba" ,"ba","aa","aba","aa","ab","bb","bb","aba"];
-console.log(array_d) ; 
-console.log(array_x) ; 
-console.log("run f1 with example 2") ; 
-console.log( f1(array_d ,array_x)) ; 
-
-
-
-// test 2 
 function f2 (a) {
-  var array_result = [] ; 
- 
-  var nbr = a;
-  patt1 =  /-\d*\.\d+|\d+/g; 
-  array_result = nbr.match(patt1);
-  for (ii=0 ; ii<array_a.length ;ii++ ) {
-    array_result [ii] =  parseFloat(array_result[ii]);
+    return  a.match(/-\d*\.\d+|\d+/g).map( value => parseFloat(value));
   }
+// uncommented the test 2 and run node solution.js in the terminal
 
-return array_result ; 
-}
-var array_a2 = "a1 12 13.b -14.5+2";
-var res = [Float32Array];
-res=f2(array_a2);
-console.log("\n") ; 
-console.log("test 2 ") ; 
-console.log( array_a2) ; 
-console.log("run function  f2") ;
-console.log( res) ; 
+// var array_a2 = "a1 12 13.b -14.5+2";
+// console.log("\n") ; 
+// console.log( array_a2) ; 
+// console.log ("run f2(array_a2)") ;
+// console.log( f2(array_a2)) ; 
 
+function f3 (o) { 
+   return  o.map(value => {  Object.freeze(value);
+           return Object.assign({ a: value.a += 1, b: value.b += "str"});      
+ }); 
+}   
 
-// test3
+//uncommented the test 3 and run node solution.js in the terminal
+// console.log("\n");
+// const a = [{a: 1, b: "str"}]
+// const b = f3(a);
+// console.log(a);
+// console.log("run  b = f3(a)");
+// console.log(f3(a));
+// console.log(a[0]==b[0]);
+// console.log(a[0].a);
+// console.log(b[0].a);  
+  
+const f = function f4 (cb = (()=>console.log("cb  is called "))){
+            let  i =0 ;  
+            cb(); 
+              setInterval(()=>  {
+              i += 100; //increment  100   
+              console.log('cb is not  called after 100 ms');
+              if( i === 300){   // condition  i =300 matches  with cb call affter 300ms
+              cb(); 
+              i=0; 
+              }},100) // cb called after 100 ms
+     }
 
-function f3 (ob) {
-  const  result=[Object];
-  result[0].a = ob[0].a+1;
-  result[0].b = ob[0].b+1;
-return result  ;
-}
-
-console.log("\n");
-console.log("test3");
-const a = [{a: 1, b: "str"}]
-const b = f3(a);
-
-console.log("run function  f3") ;
-console.log(a[0]==b[0]);
-console.log(a[0].a);
-console.log(b[0].a);
-
-// test4 
-console.log("\n");
-console.log("test4");
-
-var  myVar = setInterval(f4, 1)
-var  i = 0 ; 
-var t0 =cb = new Date().getTime();
-function f4(  cb = new Date().getTime())
-{
-  if (cb-t0 >= 300){
-    console.log("cd is called");  
-    t0=cb;
-    i++;
-  }else if ( cb-t0 < 300 && cb-t0>280 ){
-    console.log("it’s ignored and cb isn’t called") 
-  }
-  // stop the loop after 2 cd calls
-  if(i == 2 ){
-    clearInterval(myVar);
-  }
-}
-
-
-
+//  uncommented the test 4 and run  node solution.js  in the terminal
+//  for test 4 you have to cut the loop manually  with  Ctlr + c 
+//  console.log("\n");   
+//  console.log(" run  f();");
+//  f();
+    
+    
